2668e5bad44bc48a7dd826d944fb8a77f95cd722
rokibulislam <rrcucse.11@gmail.com>
2022-07-22 22:00:21 +0600

    update env

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 //db 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/helloworldapp',{ 
+    .connect(process.env.MONGO_URI,{ 
         useNewUrlParser: true, 
         useUnifiedTopology: true 
     })
